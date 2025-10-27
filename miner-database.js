141feaa6eba602932c980f844eb37437be3e8130
samuntaqim-Tech <s.a.muntaqim@gmail.com>
2025-10-28 01:30:47 +0500

    Create miner-database.js

---
+++ miner-database.js
@@ -0,0 +1,13 @@
+const MINER_DATABASE = [
+    {
+        id: 1,
+        name: "Antminer S19 XP",
+        algorithm: "SHA-256",
+        hashrate: "140 TH/s",
+        power: "3010W",
+        price: "$4,200",
+        profit: "$12.45/day",
+        roi: "338 days"
+    },
+    // Add 50+ more miners
+];
