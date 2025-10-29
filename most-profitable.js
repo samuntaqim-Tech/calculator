b875553a573c2e0638c59227da654073b3a76da8
samuntaqim-Tech <s.a.muntaqim@gmail.com>
2025-10-29 20:44:33 +0500

    Delete most-profitable.js

--- most-profitable.js
+++
@@ -1,6 +0,0 @@
-// Fetch and display most profitable miners
-async function loadMostProfitable() {
-    const miners = await fetchMinerData();
-    const profitable = miners.sort((a, b) => b.dailyProfit - a.dailyProfit);
-    displayProfitabilityRanking(profitable.slice(0, 20));
-}
